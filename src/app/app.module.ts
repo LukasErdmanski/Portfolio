1e45db0ce0c31565dd2e54508345e14b8ff78c51
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2024-06-17 18:00:14 +0200

    addes animations to start-screen and moveToSection method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StartComponent,
-    StartScreenComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, StartComponent, StartScreenComponent],
+  imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule],
+  providers: [provideAnimations()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
