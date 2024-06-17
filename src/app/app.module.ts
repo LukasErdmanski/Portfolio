1c490714952e89e9fc9ec8c7da5c674e34aabb15
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2024-06-17 11:59:19 +0200

    started start-screen component; added style for it; added headline FD LE in template; added global style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartComponent
+    StartComponent,
+    StartScreenComponent
   ],
   imports: [
     BrowserModule,
