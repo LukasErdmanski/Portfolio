c9b513f76db908b9bdcf90a9e2c5f2d8bfd493f6
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2024-07-04 06:07:57 +0200

    added changeLanguage method and angular animation for navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  animations: [
+    trigger('openClose', [
+      state(
+        'void',
+        style({
+          transform: 'translateY(-100%)',
+          opacity: 0,
+        })
+      ),
+      transition(':enter', [animate('400ms 100ms ease-in-out')]),
+      transition(':leave', [animate('225ms ease-in')]),
+    ]),
+  ],
 })
 export class NavbarComponent {
+  @ViewChild('habBtn') habBtn!: HTMLElement;
 
+  protected changeLang(language: string): void {
+    const langaugeElements: NodeListOf<Element> =
+      document.querySelectorAll('.lang');
+    langaugeElements.forEach((languageElement: Element) =>
+      languageElement.classList.remove('checked')
+    );
+
+    document.querySelector('.' + language)?.classList.add('checked');
+
+    if (language === 'en') console.log('Current language is EN');
+    else console.log('Current language is DE');
+  }
 }
