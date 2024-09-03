c8626e45c4bf3e980b3d4aa5695369e84ba19ef8
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2024-09-03 11:06:48 +0200

    added rotate-device-info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import {
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { DOCUMENT } from '@angular/common';
+import { RotateDeviceInfoComponent } from './components/rotate-device-info/rotate-device-info.component';
 
 /**
  * Factory function for the TranslateLoader.
@@ -61,6 +62,7 @@ export function HttpLoaderFactory(http: HttpClient, document: any) {
     HeaderComponent,
     NavmenuComponent,
     PortfolioComponent,
+    RotateDeviceInfoComponent,
     SkillsComponent,
     SocialsComponent,
     MainContentComponent,
---
+++ src/app/components/rotate-device-info/rotate-device-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+/**
+ * A component to display information and prompt the user to rotate their device.
+ * This  application that have a preferred orientation.
+ */
+@Component({
+    selector: 'app-rotate-device-info',
+    templateUrl: './rotate-device-info.component.html',
+    styleUrls: ['./rotate-device-info.component.scss'],
+})
+export class RotateDeviceInfoComponent {}
